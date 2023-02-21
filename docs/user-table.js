e472f94cb5c7991389d67ec94a75333cb27de488
lasporrasdef1 <125993131+lasporrasdef1@users.noreply.github.com>
2023-02-21 17:18:58 +0100

    Update user-table.js

--- docs/user-table.js
+++ docs/user-table.js
@@ -1,32 +1,20 @@
-const userTableBody = document.getElementById("user-table-body");
+const userTable = document.querySelector('#user-table tbody');
 
-// Function to retrieve the users and their points from users.json
-function getUsers() {
-  return fetch("users.json").then((response) => response.json());
-}
-
-// Function to create a row in the user table
-function createRow(username, points) {
-  const row = document.createElement("tr");
-  const usernameCell = document.createElement("td");
-  const pointsCell = document.createElement("td");
-  usernameCell.textContent = username;
-  pointsCell.textContent = points;
-  row.appendChild(usernameCell);
-  row.appendChild(pointsCell);
-  return row;
-}
-
-// Function to display the users and their points in the user table
-function displayUsers() {
-  getUsers().then((users) => {
-    userTableBody.innerHTML = ""; // Clear existing rows
-    Object.keys(users).forEach((username) => {
-      const points = users[username];
-      const row = createRow(username, points);
-      userTableBody.appendChild(row);
+fetch('users.json')
+  .then(response => response.json())
+  .then(users => {
+    users.forEach(user => {
+      const row = document.createElement('tr');
+      const usernameCell = document.createElement('td');
+      usernameCell.textContent = user.username;
+      const pointsCell = document.createElement('td');
+      pointsCell.textContent = user.points;
+      });
+      row.appendChild(usernameCell);
+      row.appendChild(pointsCell);
+      userTable.appendChild(row);
     });
+  })
+  .catch(error => {
+    console.error('Error retrieving user data:', error);
   });
-}
-
-displayUsers(); // Display the users on page load
